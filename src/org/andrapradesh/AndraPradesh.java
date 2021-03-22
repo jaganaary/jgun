9a13804348ebadbc559d3a0e34362c331735a8b6
77hariprasad77 <77hariprasad77@gmail.com>
2021-03-22 13:33:53 +0530

    from branch1

--- src/org/andrapradesh/AndraPradesh.java
+++ src/org/andrapradesh/AndraPradesh.java
@@ -5,5 +5,14 @@ public class AndraPradesh {
 		System.out.println("telugu spoken by andhra people");
 
 	}
+	
+	public static void main(String[] args) {
+		AndraPradesh a = new AndraPradesh();
+		a.telugu();
+	}
+	
+	
+	
+	
 
 }
